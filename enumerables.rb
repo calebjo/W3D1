cdcd544dac9288fb87b54977806f17c87d63386c
Caleb Jones <caleb.ri.jones@gmail.com>
2021-10-18 15:29:30 -0400

    Add methods for enumerables.rb

--- enumerables.rb
+++ enumerables.rb
@@ -1,4 +1,4 @@
-
+require 'byebug'
 
 class Array
 
@@ -26,13 +26,45 @@ class Array
         new_arr
     end
 
-    
+    def my_reject(&prc)
+        new_arr = []
 
+        self.my_each do |ele|
+            if !prc.call(ele)
+                new_arr << ele
+            end
+        end
 
+        new_arr
+    end
 
+    def my_any?(&prc)
+        self.my_each do |ele|
+            return true if prc.call(ele)
+        end
+        false
+    end
 
+    def my_all?(&prc)
+        self.my_each do |ele|
+            return false if !prc.call(ele)
+        end
+        true
+    end
 
+    def my_flatten
+        flat_arr = []
+        self.each do |ele|
+            if !ele.is_a?(Array) 
+                flat_arr << ele
+            else
+                flat_arr.concat(ele.my_flatten)
+            end
+        end
+        flat_arr
+    end
 end
 
 
+print [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 
